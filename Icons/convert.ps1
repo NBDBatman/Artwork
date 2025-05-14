68e808fe19cc13f9da62f870c1db0b65a32011ed
FatalMerlin <merlin.brandes@gmail.com>
2025-05-15 00:44:35 +0200

    feat: import artwork

---
+++ Icons/convert.ps1
@@ -0,0 +1,1 @@
+wsl convert *.png favicon.ico
---
+++ Icons/export.ps1
@@ -0,0 +1,1 @@
+256,96,80,72,64,60,48,40,36,32,30,24,20,16 | % { & "C:\Program Files\Inkscape\bin\inkscape.exe" -w $_ -h $_ -o "./$_.png" '..\Arkanis Icon.svg' }
