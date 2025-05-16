2fd93595e9843e61095c216266007f532af924c5
FatalMerlin <merlin.brandes@gmail.com>
2025-05-16 20:46:23 +0200

    feat: add new & organize icons

--- Icons/convert.ps1
+++ Icons/convert.ps1
@@ -1,1 +1,5 @@
-wsl convert *.png favicon.ico
+Get-ChildItem -Directory . | ForEach-Object {
+    wsl convert ./$($_.BaseName)/*.png ./$($_.BaseName)/favicon.ico
+}
+
+# wsl convert ./Small/*.png ./Small/favicon.ico
--- Icons/export.ps1
+++ Icons/export.ps1
@@ -1,1 +1,8 @@
-256,96,80,72,64,60,48,40,36,32,30,24,20,16 | % { & "C:\Program Files\Inkscape\bin\inkscape.exe" -w $_ -h $_ -o "./$_.png" '..\Arkanis Icon.svg' }
+mkdir -Force ./Regular, ./Small, ./Small_Outlined, ./Small_Inverted | Out-Null
+
+256, 96, 80, 72, 64, 60, 48, 40, 36, 32, 30, 24, 20, 16 | ForEach-Object {
+    & "C:\Program Files\Inkscape\bin\inkscape.exe" -w $_ -h $_ -o "./Regular/$_.png" './Arkanis_Icon_Regular.svg'
+    & "C:\Program Files\Inkscape\bin\inkscape.exe" -w $_ -h $_ -o "./Small/$_.png" './Arkanis_Icon_Small.svg'
+    & "C:\Program Files\Inkscape\bin\inkscape.exe" -w $_ -h $_ -o "./Small_Outlined/$_.png" './Arkanis_Icon_Small_Outlined.svg'
+    & "C:\Program Files\Inkscape\bin\inkscape.exe" -w $_ -h $_ -o "./Small_Inverted/$_.png" './Arkanis_Icon_Small_Inverted.svg'
+}
